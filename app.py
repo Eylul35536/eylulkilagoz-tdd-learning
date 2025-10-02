48e9f74f02aec106527450fe397998a1ada26e78
eylul <eylul.kilagoz@akademiata.edu.pl>
2025-10-02 17:49:57 +0000

    tdd-learning

---
+++ app.py
@@ -0,0 +1,5 @@
+def greeting():
+    return "Hello, I am Eylul. How can i help you today ?"
+
+if __name__ == "__main__":
+    print(greeting())
---
+++ test_app.py
@@ -0,0 +1,4 @@
+from app import greeting
+
+def test_greeting():
+    assert greeting() == "Hello, I am Eylul. How can i help you today ?"
